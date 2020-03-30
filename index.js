428a32f0cd961388d65ab5bf7756eff1b00d71dc
Diaa Kasem <me@diaa.me>
2020-03-30 11:07:27 +0200

    First commit

---
+++ index.js
@@ -0,0 +1,77 @@
+import axios from 'axios';
+import _ from 'lodash';
+
+const url = 'https://www.worldometers.info/coronavirus/coronavirus-death-toll/';
+const regex = /data\:\s*\[([0-9]+,\s?)+[0-9]+\]/g;
+async function getInfo() {
+    return axios.get(url)
+    .then((res) => {
+        return res.data;
+    }).then((html) => {
+        const results = html.match(regex);
+        return results;
+    }).then((res) => {
+        return _.map(res, (str) => {
+            return JSON.parse(_.last(str.split(':')));
+        });
+    }).then((res) => {
+        return {
+            totalDeaths: _.first(res),
+            dailyDeaths: _.last(res),
+        };
+    }).then((res) => {
+        res.lastDailyDeath = _.last(res.dailyDeaths);
+        res.lastTotalDeath = _.last(res.totalDeaths);
+        res.dailyDeathRateOfChange = _.compact(_.map(res.dailyDeaths, (d, i) => {
+            if (! (i && (i-1))) {
+                return 0;
+            }
+            const out = (d / res.dailyDeaths[i-1]);
+            // console.log(res.dailyDeaths[i-1], d, out)
+            return out;
+        }));
+        const recentDays = 10;
+        res.recentDailyDeathRateOfChange = _.takeRight(res.dailyDeathRateOfChange, recentDays)
+        res.dailyDeathTotalChange = res.dailyDeathRateOfChange.reduce((x, y) => x + y);
+        res.dailyDeathChangeRatio = res.dailyDeathTotalChange / res.dailyDeathRateOfChange.length;
+        res.recentDailyDeathTotalChange = res.recentDailyDeathRateOfChange.reduce((x, y) => x + y);
+        res.recentDailyDeathChangeRatio = res.recentDailyDeathTotalChange / res.recentDailyDeathRateOfChange.length;
+        return res;
+    }).then((res) => {
+        // console.log(res);
+        return res;
+    }).catch((err) => {
+        console.error(err);
+    });
+}
+
+async function deathAfter(days) {
+    const info = await getInfo();
+    let newDailyDeath = info.lastDailyDeath;
+    let newTotalDeath = info.lastTotalDeath;
+    let newRecentDailyDeath = info.lastDailyDeath;
+    let newRecentTotalDeath = info.lastTotalDeath;
+    _.times(days, () => {
+        newDailyDeath *= info.dailyDeathChangeRatio;
+        newTotalDeath += newDailyDeath;
+        newRecentDailyDeath *= info.recentDailyDeathChangeRatio;
+        newRecentTotalDeath += newRecentDailyDeath;
+    });
+    return {
+        lastDailyDeath: Math.round(info.lastDailyDeath),
+        lastTotalDeath: Math.round(info.lastTotalDeath),
+        newDailyDeath: Math.round(newDailyDeath),
+        newTotalDeath: Math.round(newTotalDeath),
+        newRecentDailyDeath: Math.round(newRecentDailyDeath),
+        newRecentTotalDeath: Math.round(newRecentTotalDeath),
+    };
+}
+deathAfter(10).then((res) => {
+    console.log(`
+                Last Daily Death: ${res.lastDailyDeath}, Total Death : ${res.lastTotalDeath}
+
+                Future Daily Death: ${res.newDailyDeath}, Total Death : ${res.newTotalDeath}
+
+                Future Based on Recent Daily Death: ${res.newRecentDailyDeath}, Total Death : ${res.newRecentTotalDeath}
+                `)
+});
