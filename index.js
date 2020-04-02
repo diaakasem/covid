c7955d1bbf82357d861832888a36f7496ff01003
Diaa Kasem <me@diaa.me>
2020-04-02 03:48:00 +0200

    Adding Cases info

--- index.js
+++ index.js
@@ -2,10 +2,11 @@ import axios from 'axios';
 import _ from 'lodash';
 
 const url = 'https://www.worldometers.info/coronavirus/coronavirus-death-toll/';
+const url2 = 'https://www.worldometers.info/coronavirus/coronavirus-cases/'
 const regex = /data\:\s*\[([0-9]+,\s?)+[0-9]+\]/g;
 const futureDays = 10;
 
-async function getInfo() {
+async function getInfo(url) {
     return axios.get(url)
     .then((res) => {
         return res.data;
@@ -18,26 +19,26 @@ async function getInfo() {
         });
     }).then((res) => {
         return {
-            totalDeaths: _.first(res),
-            dailyDeaths: _.last(res),
+            total: _.first(res),
+            daily: _.last(res),
         };
     }).then((res) => {
-        res.lastDailyDeath = _.last(res.dailyDeaths);
-        res.lastTotalDeath = _.last(res.totalDeaths);
-        res.dailyDeathRateOfChange = _.compact(_.map(res.dailyDeaths, (d, i) => {
+        res.lastDaily = _.last(res.daily);
+        res.lastTotal = _.last(res.total);
+        res.dailyRateOfChange = _.compact(_.map(res.daily, (d, i) => {
             if (! (i && (i-1))) {
                 return 0;
             }
-            const out = (d / res.dailyDeaths[i-1]);
-            // console.log(res.dailyDeaths[i-1], d, out)
+            const out = (d / res.daily[i-1]);
+            // console.log(res.dailys[i-1], d, out)
             return out;
         }));
         const recentDays = 10;
-        res.recentDailyDeathRateOfChange = _.takeRight(res.dailyDeathRateOfChange, recentDays)
-        res.dailyDeathTotalChange = res.dailyDeathRateOfChange.reduce((x, y) => x + y);
-        res.dailyDeathChangeRatio = res.dailyDeathTotalChange / res.dailyDeathRateOfChange.length;
-        res.recentDailyDeathTotalChange = res.recentDailyDeathRateOfChange.reduce((x, y) => x + y);
-        res.recentDailyDeathChangeRatio = res.recentDailyDeathTotalChange / res.recentDailyDeathRateOfChange.length;
+        res.recentDailyRateOfChange = _.takeRight(res.dailyRateOfChange, recentDays)
+        res.dailyTotalChange = res.dailyRateOfChange.reduce((x, y) => x + y);
+        res.dailyChangeRatio = res.dailyTotalChange / res.dailyRateOfChange.length;
+        res.recentDailyTotalChange = res.recentDailyRateOfChange.reduce((x, y) => x + y);
+        res.recentDailyChangeRatio = res.recentDailyTotalChange / res.recentDailyRateOfChange.length;
         return res;
     }).then((res) => {
         // console.log(res);
@@ -47,35 +48,46 @@ async function getInfo() {
     });
 }
 
-async function deathAfter(days) {
-    const info = await getInfo();
-    let newDailyDeath = info.lastDailyDeath;
-    let newTotalDeath = info.lastTotalDeath;
-    let newRecentDailyDeath = info.lastDailyDeath;
-    let newRecentTotalDeath = info.lastTotalDeath;
+async function deathAfter(url, days) {
+    const info = await getInfo(url);
+    let newDaily = info.lastDaily;
+    let newTotal = info.lastTotal;
+    let newRecentDaily = info.lastDaily;
+    let newRecentTotal = info.lastTotal;
     _.times(days, () => {
-        newDailyDeath *= info.dailyDeathChangeRatio;
-        newTotalDeath += newDailyDeath;
-        newRecentDailyDeath *= info.recentDailyDeathChangeRatio;
-        newRecentTotalDeath += newRecentDailyDeath;
+        newDaily *= info.dailyChangeRatio;
+        newTotal += newDaily;
+        newRecentDaily *= info.recentDailyChangeRatio;
+        newRecentTotal += newRecentDaily;
     });
     return {
-        lastDailyDeath: Math.round(info.lastDailyDeath),
-        lastTotalDeath: Math.round(info.lastTotalDeath),
-        newDailyDeath: Math.round(newDailyDeath),
-        newTotalDeath: Math.round(newTotalDeath),
-        newRecentDailyDeath: Math.round(newRecentDailyDeath),
-        newRecentTotalDeath: Math.round(newRecentTotalDeath),
+        lastDaily: Math.round(info.lastDaily),
+        lastTotal: Math.round(info.lastTotal),
+        newDaily: Math.round(newDaily),
+        newTotal: Math.round(newTotal),
+        newRecentDaily: Math.round(newRecentDaily),
+        newRecentTotal: Math.round(newRecentTotal),
     };
 }
 function inK(v) {
     return Math.round(v/1000);
 }
-deathAfter(futureDays).then((res) => {
-    const futureDailyAvg = (res.newDailyDeath + res.newRecentDailyDeath) / 2;
-    const futureTotalAvg = (res.newTotalDeath + res.newRecentTotalDeath) / 2;
+Promise.all([
+    deathAfter(url, futureDays),
+    deathAfter(url2, futureDays)
+]).then((results) => {
+    const death = _.first(results);
+    const cases = _.last(results);
+    const futureDailyAvgDeath = (death.newDaily + death.newRecentDaily) / 2;
+    const futureTotalAvgDeath = (death.newTotal + death.newRecentTotal) / 2;
+    const futureDailyAvgCases = (cases.newDaily + cases.newRecentDaily) / 2;
+    const futureTotalAvgCases = (cases.newTotal + cases.newRecentTotal) / 2;
     console.log(`
-    Last Daily Death:\t ${inK(res.lastDailyDeath)}K,\t In ${futureDays} Days: \t ${inK(futureDailyAvg)}K
-    Total Death:\t ${inK(res.lastTotalDeath)}K,\t In ${futureDays} Days: \t ${inK(futureTotalAvg)}K
+    Last Daily Death:\t ${inK(death.lastDaily)}K,\t In ${futureDays} Days: \t ${inK(futureDailyAvgDeath)}K
+    Total Death:\t ${inK(death.lastTotal)}K,\t In ${futureDays} Days: \t ${inK(futureTotalAvgDeath)}K
+    `)
+    console.log(`
+    Last Daily Cases:\t ${inK(cases.lastDaily)}K,\t In ${futureDays} Days: \t ${inK(futureDailyAvgCases)}K
+    Total Cases:\t ${inK(cases.lastTotal)}K,\t In ${futureDays} Days: \t ${inK(futureTotalAvgCases)}K
     `)
 });
